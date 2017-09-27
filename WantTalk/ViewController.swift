48abf1787bbb263708f3cd69d74bf231879da6bc
李依 <00332812@shqnb00001795.local>
2017-09-27 10:57:30 +0800

    Initial Commit

---
+++ WantTalk/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WantTalk
+//
+//  Created by 李依 on 2017/9/27.
+//  Copyright © 2017年 李依. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
